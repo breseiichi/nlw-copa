2261a5efd6016044837d81ee994d1898a6464609
Brenda Seiichi <brenda_seiichi@hotmail.com>
2022-11-05 15:46:29 -0300

    :tada: initial commit

---
+++ main.js
@@ -0,0 +1,49 @@
+function createGame(player1, hour, player2) {
+  return `
+  <li>
+    <img src="./assets/icon-${player1}.svg" alt="${player1}" />
+    <strong>${hour}</strong>
+    <img src="./assets/icon-${player2}.svg" alt="${player2}" />
+  </li>
+  `
+}
+
+//Toda vez que eu executar esta função, ela vai retornar este pedaço de código para mim
+function createCard(date, day, games) {
+  return `
+  <div class="card">
+      <h2>${date} <span>${day}</span></h2>
+      <ul>
+        ${games}
+      </ul>
+    </div>
+  `
+}
+
+/*documento- procura pelo seletor app- faz uma 
+inserção do html */
+document.querySelector("#cards").innerHTML =
+  createCard(
+    "24/11",
+    "quinta-feira",
+    createGame("switzerland", "07:00", "cameroon") +
+      createGame("uruguay", "10:00", "southkorea") +
+      createGame("portugal", "13:00", "ghana") +
+      createGame("brazil", "16:00", "serbia")
+  ) +
+  createCard(
+    "28/11",
+    "segunda-feira",
+    createGame("cameroon", "07:00", "serbia") +
+      createGame("southkorea", "10:00", "ghana") +
+      createGame("brazil", "13:00", "switzerland") +
+      createGame("portugal", "16:00", "uruguay")
+  ) +
+  createCard(
+    "02/12",
+    "sexta-feira",
+    createGame("southkorea", "12:00", "portugal") +
+      createGame("ghana", "12:00", "uruguay") +
+      createGame("serbia", "16:00", "switzerland") +
+      createGame("brazil", "16:00", "cameroon")
+  )
